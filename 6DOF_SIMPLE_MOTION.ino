a9b1a53501e2b558541b6838214bfe9920ef831b
devika-dudo <144927310+devika-dudo@users.noreply.github.com>
2025-05-19 14:01:05 +0530

    Update 6DOF_SIMPLE_MOTION.ino

--- 6DOF_SIMPLE_MOTION.ino
+++ 6DOF_SIMPLE_MOTION.ino
@@ -7,12 +7,12 @@ const int dirPins[6] = {9, 11, 24, 28, 37, 35};  // Direction control pins
 const int pwmPins[6] = {10, 12, 25, 29, 36, 33}; // PWM control pins  
 
 // Encoder pins (A channels must be interrupt-capable)
-const int encoderAPins[6] = {1, 3, 5, 7, 41,22}; // Interrupt pins
-const int encoderBPins[6] = {2, 4, 6, 8, 40,23}; // Digital pins
+const int encoderAPins[6] = {19, 1, 22, 14, 41,6}; // Interrupt pins
+const int encoderBPins[6] = {18, 2, 23, 13, 40,7}; // Digital pins
 
 // --- Motor and Encoder Variables ---
 volatile long encoderCounts[6] = {0, 0, 0, 0, 0, 0};
-bool invertEncoderCounting[6] = {false, false, false, false, false, false};
+bool invertEncoderCounting[6] = {true, true, false, false, false, false};
 
 // --- PID Control Constants ---
 // These should be tuned for your specific motors and application
